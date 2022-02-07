65679407d6bae6e2fb5e994167fd1d2d2ef58acd
Bruno Del Piero <bdelpiero93@gmail.com>
2022-02-06 21:49:13 -0300

    factorial

---
+++ src/unit1.1/factorial.ts
@@ -0,0 +1,8 @@
+import { promptRevursively } from "../utils/run";
+
+function factorial(n: number): number {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+promptRevursively(factorial, "enter an int: ", "please enter a valid integer");
