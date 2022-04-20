8ad70f5aaf0c87371decaa8cc552c8ed1e2692c8
Bruno Del Piero <bdelpiero93@gmail.com>
2022-04-19 22:50:55 -0300

    add Bag

---
+++ src/chapter1/section3/Bag/Bag.test.ts
@@ -0,0 +1,28 @@
+import { isTemplateExpression } from "typescript";
+import Bag from "./Bag";
+
+describe("Bag", () => {
+  let bag: Bag<string>;
+
+  beforeEach(() => {
+    bag = new Bag();
+  });
+
+  it("is created empty", () => {
+    expect(bag.isEmpty()).toBe(true);
+  });
+
+  it("can add to the bag", () => {
+    bag.add("🥑");
+    bag.add("🌽");
+  });
+
+  it("can iterate through the bag with for... of", () => {
+    const items = ["h", "e", "l", "l", "o"];
+    items.forEach((item) => bag.add(item));
+
+    for (const item of bag) {
+      expect(items.indexOf(item ?? "")).not.toBe(-1);
+    }
+  });
+});
---
+++ src/chapter1/section3/Bag/Bag.ts
@@ -0,0 +1,31 @@
+import Node from "../Node";
+
+/**
+ * Most efficient implementation, using a linked list:
+ * - It can be used for any type of data.
+ * - The space required is always proportional to the size of the collection.
+ * - The time per operation is always independent of the size of the collection
+ */
+export default class Bag<Item> implements Bag<Item>, Iterable<Item | null> {
+  private first: Node<Item> | null;
+
+  constructor() {
+    this.first = null;
+  }
+
+  public isEmpty() {
+    return this.first === null;
+  }
+
+  public add(item: Item) {
+    const oldFirst = this.first;
+    this.first = new Node(item, oldFirst);
+  }
+
+  // Generator that replaces Iterator implementation
+  *[Symbol.iterator]() {
+    for (let node = this.first; node !== null; node = node.next) {
+      yield node.value;
+    }
+  }
+}
